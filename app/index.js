cd6cd5381f53d66ea5cc02369429f4012e585426
enrique novoa <luisenriquenovoa@gmail.com>
2018-01-23 21:12:30 -0800

    1st

---
+++ app/index.js
@@ -0,0 +1,18 @@
+var React = required('react');
+var ReactDOM = required('react-dom');
+require('./index.css');
+
+// Components Properties
+// - State
+// - Lifecycle Events
+// - UI
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
