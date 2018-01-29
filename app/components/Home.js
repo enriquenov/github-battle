4a9fa3bff663cfdcd480871b08d983f7fae61584
enrique novoa <luisenriquenovoa@gmail.com>
2018-01-28 19:58:07 -0800

    15th

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,12 +2,12 @@ var React = require('react');
 var Link = require('react-router-dom').Link;
 
 
-class Home extends React.Component() {
+class Home extends React.Component {
   render() {
     return (
       <div className="home-container">
         <h1>Github Battle: Battle your friends... and stuff.</h1>
-        
+
         <Link className="button" to="/battle">
           Battle
         </Link>
