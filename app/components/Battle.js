02c27ef45aafa38cb46e93de2b73857424ffd34a
enrique novoa <luisenriquenovoa@gmail.com>
2018-01-28 20:51:21 -0800

    17th

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,10 +1,57 @@
 var React = require('react');
+var PropTypes = require('prop-types');
 
-class Battle extends React.Component {
+class PlayerInput extends React.Component {
   render() {
     return (
+
+    )
+  }
+}
+
+PlayerInput.propTypes = {
+
+}
+
+class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(id, username) {
+    this.setState(function() {
+      var newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] = `https://github.com/${username}.png?size=200`;
+      return newState;
+    })
+  }
+
+  render() {
+    var playerOneName = this.state.playerOneName;
+    var playerTwoName = this.state.playerTwoName;
+
+    return (
       <div>
-        Battle
+        <div className="row">
+          {!playerOneName &&
+            <PlayerInput
+              id="PlayerOne"
+              label="Player One"
+              onSubmit={this.handleSubmit}/>}
+          {!playerTwoName &&
+            <PlayerInput
+              id="PlayerTwo"
+              label="Player Two"
+              onSubmit={this.handleSubmit}/>}
+        </div>
       </div>
     )
   }
