53bea2df198881e2ac9927f4c6aa279efd447f3c
enrique novoa <luisenriquenovoa@gmail.com>
2018-01-28 19:30:42 -0800

    14th

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,13 +1,23 @@
 var React = require('react');
-// var Link = require('react-router-dom').Link;
+// var Link = require('react-router-dom').Link;  NavLink is better for an active element.
 var NavLink = require('react-router-dom').NavLink;
 
 function Nav() {
   return (
     <ul className="nav">
       <li>
-        <NavLink activeClassName="active" to="/">
+        <NavLink exact activeClassName="active" to="/">
           Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="active" to="/battle">
+          Battle
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="active" to="/popular">
+          Popular
         </NavLink>
       </li>
     </ul>
