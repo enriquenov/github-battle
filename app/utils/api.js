fc359e19d32b4678e17aaec6382df9e1d0277ce7
enrique novoa <luisenriquenovoa@gmail.com>
2018-02-04 22:40:14 -0800

    29th

--- app/utils/api.js
+++ app/utils/api.js
@@ -37,14 +37,28 @@ function getUserData(player) {
   return axios.all([
     getProfile(player),
     getRepos(player)
-  ]).then(function() {
+  ]).then(function(data) {
+    var profile = data[0];
+    var repos = data[1];
 
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    }
   })
+}
+
+function sortPlayers(players) {
+  return players.sort(function(a, b){
+    return b.score - a.score;
+  });
 }
 
 module.exports = {
   battle: function(players) {
-
+    return axios.all(players.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError)
   },
   fetchPopularRepos: function(language) {
     var encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
